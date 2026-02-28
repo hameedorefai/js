ddfac69065bc4e3fe702b9e425c7d8cb9cb8daef
Mahmoud Refai <98220943+hameedorefai@users.noreply.github.com>
2026-02-28 03:15:46 +0200

    Refactor getFileData and related functions

--- file.js
+++ file.js
@@ -14,7 +14,7 @@ async function getFileData() {
     const courseId = await getParam('id') || null;
     if (courseId) {
         try {
-            const response = await fetch('https://zplatform.azurewebsites.net/api/files/' + courseId);
+            const response = await fetch('https://zplatform2.azurewebsites.net/api/files/' + courseId);
 
             if (!response.ok) {
                 // محاولة قراءة نص الرسالة من الخادم
@@ -373,8 +373,8 @@ async function sendVote(dataPost) {
         let response;
         if (dataPost.deleted) {
             const deleteUrl = dataPost.deleted === 'upVote'
-                ? `https://zplatform.azurewebsites.net/api/FileUpVote/${dataPost.fileId}/upvote`
-                : `https://zplatform.azurewebsites.net/api/FileDownVote/${dataPost.fileId}/downvote`;
+                ? `https://zplatform2.azurewebsites.net/api/FileUpVote/${dataPost.fileId}/upvote`
+                : `https://zplatform2.azurewebsites.net/api/FileDownVote/${dataPost.fileId}/downvote`;
 
             response = await fetch(deleteUrl, {
                 method: "DELETE",
@@ -390,8 +390,8 @@ async function sendVote(dataPost) {
         }
         if (dataPost.vote) {
             const postUrl = dataPost.vote === 'upVote'
-                ? `https://zplatform.azurewebsites.net/api/FileUpVote/${dataPost.fileId}/upvote`
-                : `https://zplatform.azurewebsites.net/api/FileDownVote/${dataPost.fileId}/downvote`;
+                ? `https://zplatform2.azurewebsites.net/api/FileUpVote/${dataPost.fileId}/upvote`
+                : `https://zplatform2.azurewebsites.net/api/FileDownVote/${dataPost.fileId}/downvote`;
 
             response = await fetch(postUrl, {
                 method: "POST",
@@ -412,7 +412,7 @@ let downvotes = 0;
 let upvotes = 0;
         
         try {
-            const response = await fetch(`https://zplatform.azurewebsites.net/api/FileDownVote/${dataPost.fileId}/downvotes/count`);
+            const response = await fetch(`https://zplatform2.azurewebsites.net/api/FileDownVote/${dataPost.fileId}/downvotes/count`);
             if (!response.ok) {
                 return true;
             }
@@ -423,7 +423,7 @@ let upvotes = 0;
             throw new Error(error);
         }
         try {
-            const response = await fetch(`https://zplatform.azurewebsites.net/api/FileUpVote/${dataPost.fileId}/upvotes/count`);
+            const response = await fetch(`https://zplatform2.azurewebsites.net/api/FileUpVote/${dataPost.fileId}/upvotes/count`);
             if (!response.ok) {
                 return true;
             }
@@ -462,6 +462,7 @@ function openPDFModal(pdfPath) {
     const PDFModal = new bootstrap.Modal(document.getElementById('pdfModal'));
     PDFModal.show();
 }
+
 
 
 
